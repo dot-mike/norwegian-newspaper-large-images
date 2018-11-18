4bb12f22a51ea6f71e9d25eaf685862be4777a90
Ondkloss <ondkloss@gmail.com>
2018-11-18 13:47:12 +0100

    Added zoom for images found on https://morenytt.alda.no, which is mostly accessed if you don't have a subscription.

--- src/content.js
+++ src/content.js
@@ -1,10 +1,23 @@
 'use strict';
 
 function switchImages() {
-    const bilete = Array.prototype.slice.call(document.getElementsByClassName("js-require-widget responsiveImage"), 0);
-    bilete.forEach(element => {
+    // Images mostly found on https://www.morenytt.no
+    const dynamicImages = Array.prototype.slice.call(document.getElementsByClassName("js-require-widget responsiveImage"), 0);
+    dynamicImages.forEach(element => {
         if (element.getAttribute('data-src').includes('{width}')) {
             element.setAttribute('data-src', element.getAttribute('data-src').replace(/{width}/g, '1980'));
+        }
+    });
+
+    // Images mostly found on https://morenytt.alda.no
+    const staticImages = Array.prototype.slice.call(document.getElementsByClassName("img-responsive"), 0);
+    staticImages.forEach(element => {
+        if (RegExp(/\/w[0-9]+(-\w+)*\//g).test(element.getAttribute('src'))) {
+            element.setAttribute('data-action', 'zoom');
+            element.parentElement.style.overflow = 'visible';
+            element.setAttribute('src', element.getAttribute('src').replace(/\/w[0-9]+(-\w+)*\//g, function (match, p1) {
+                return '/w1980' + p1 + '/';
+            }));
         }
     });
 }
