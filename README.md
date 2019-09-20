2ae3055c1113734635c559c11b110fefe2f9d39f
Halvor Holsten Strand <halvor.holsten.strand@gmail.com>
2019-09-20 07:39:49 +0200

    Another refactor and rebrand to cover Norwegian papers in general. Added support for some other websites.

--- README.md
+++ README.md
@@ -1,46 +1,58 @@
-# Polaris media large images
+# Norwegian newspaper large images
 
-Adds an element in your context menu to "Open large image", when right clicking an image on a Polaris Media newspaper website. Covered papers ([see Polaris Medias own list](http://www.polarismedia.no/vare-selskaper/)):
+Adds an element in your context menu to "Open large image", when right clicking an image on many Norwegian newspaper website.
 
-* Adresseavisen
-* Avisa Sør-Trøndelag
-* Bladet
-* Brønnøysunds avis
-* Fosna-Folket
-* Hitra-Frøya
-* Innherred
-* Opdalingen
-* Trønderbladet
-* Altaposten
-* Andøyposten
-* Folkebladet
-* Framtid i Nord
-* Harstad Tidende
-* iTromsø
-* Vesteraalens Avis
-* Vesterålen Online
-* Åndalsnes Avis
-* Driva
-* Dølen
-* Fjordabladet
-* Fjordenes Tidende
-* Fjordingen
-* Fjuken
-* Møre-Nytt
-* Romsdals Budstikke
-* Sunnmøringen
-* Sunnmørsposten
-* Vestlandsnytt
-* Vigga
-* Vikebladet
+Covered Polaris Media newspapers ([see Polaris Medias own list](http://www.polarismedia.no/vare-selskaper/)):
 
+- Adresseavisen
+- Avisa Sør-Trøndelag
+- Bladet
+- Brønnøysunds avis
+- Fosna-Folket
+- Hitra-Frøya
+- Innherred
+- Opdalingen
+- Trønderbladet
+- Altaposten
+- Andøyposten
+- Folkebladet
+- Framtid i Nord
+- Harstad Tidende
+- iTromsø
+- Vesteraalens Avis
+- Vesterålen Online
+- Åndalsnes Avis
+- Driva
+- Dølen
+- Fjordabladet
+- Fjordenes Tidende
+- Fjordingen
+- Fjuken
+- Møre-Nytt
+- Romsdals Budstikke
+- Sunnmøringen
+- Sunnmørsposten
+- Vestlandsnytt
+- Vigga
+- Vikebladet
 
+Other Norwegian newspapers:
+
+- Digi
+- Teknisk Ukeblad
+- Inside Telecom
+- Veier24
+- Dagbladet
 
 Released on [Chrome Web Store](https://chrome.google.com/webstore/detail/eohpfbapbmhblpjcnjfikpmcdkkpkihg) and [Firefox Add-ons](https://addons.mozilla.org/en-US/firefox/addon/polaris-media-large-images/).
 
-Functionality tested on 2019-09-18.
+Functionality tested on 2019-09-19.
 
 ## Changelog
+
+### 1.6
+
+Extended support to some other Norwegian newspapers.
 
 ### 1.5
 
@@ -48,7 +60,7 @@ Extension now gives a context menu option upon right clicking images on Polaris 
 
 ### < 1.5
 
-These versions injected content scripts, but as Polar Media kept changing up their DOM/scripts it didn't work well for long.
+These versions injected content scripts, but as Polaris Media kept changing up their DOM/scripts it didn't work well for long.
 
 ## License
 
--- src/background.js
+++ src/background.js
@@ -1,7 +1,7 @@
-function openLargeImage(info, tab) {
+function openLargeImage(info, tab, func) {
     console.log(info, tab);
     const srcUrl = info.srcUrl;
-    const largeImageUrl = getLargeImageUrl(srcUrl);
+    const largeImageUrl = func(srcUrl);
 
     if (largeImageUrl != null)
         openUrlInTab(largeImageUrl, tab);
@@ -9,11 +9,30 @@ function openLargeImage(info, tab) {
         openUrlInTab(srcUrl, tab)
 }
 
-function getLargeImageUrl(url) {
-    if (RegExp(/\/w[0-9]+(-\w+)*\//g).test(url)) {
-        return url.replace(/\/w[0-9]+(-\w+)*\//g, function (match, p1) {
-            return '/w1980-default/';
-        });
+// Dagbladet handler
+function getLargeDbImageUrl(url) {
+    const pattern = /\/([0-9]+)\.jpg\?[0-9a-zA-Z\.&=]+$/g;
+    const func = (x, p1) => '/' + p1 + '.jpg?imageId=' + p1 + '&x=0&y=0&cropw=0&croph=0&width=-1&height=-1&compression=0';
+    return performRegExp(url, pattern, func);
+}
+
+// Teknisk Ukeblad Media handler
+function getLargeTumImageUrl(url) {
+    const pattern = /\/(.+)\.[0-9]+x[0-9]+c\.jpg$/g;
+    const func = (x, p1) => '/' + p1 + '.jpg';
+    return performRegExp(url, pattern, func);
+}
+
+// Polar Media handler
+function getLargePmImageUrl(url) {
+    const pattern = /\/w[0-9]+(-\w+)*\//g;
+    const func = () => '/w1980-default/';
+    return performRegExp(url, pattern, func);
+}
+
+function performRegExp(value, pattern, func) {
+    if (RegExp(pattern).test(value)) {
+        return value.replace(pattern, func);
     }
 
     return null;
@@ -28,12 +47,12 @@ function openUrlInTab(url, openerTab) {
     });
 }
 
-// URL patterns grabbed from:
+// Polaris Media
 // http://www.polarismedia.no/vare-selskaper/
 chrome.contextMenus.create({
     title: chrome.i18n.getMessage("openLarge"),
     contexts: ["image"],
-    onclick: openLargeImage,
+    onclick: (info, tab) => openLargeImage(info, tab, getLargePmImageUrl),
     documentUrlPatterns: [
         // Polar Media Midt-Norge
         "*://*.adressa.no/*",
@@ -69,5 +88,28 @@ chrome.contextMenus.create({
         "*://*.vestlandsnytt.no/*",
         "*://*.vigga.no/*",
         "*://*.vikebladet.no/*"
+    ]
+});
+
+// Teknisk Ukeblad Media
+chrome.contextMenus.create({
+    title: chrome.i18n.getMessage("openLarge"),
+    contexts: ["image"],
+    onclick: (info, tab) => openLargeImage(info, tab, getLargeTumImageUrl),
+    documentUrlPatterns: [
+        "*://*.tu.no/*",
+        "*://*.digi.no/*",
+        "*://www*.insidetelecom.no/*",
+        "*://*.veier24.no/*"
+    ]
+});
+
+// Dagbladet
+chrome.contextMenus.create({
+    title: chrome.i18n.getMessage("openLarge"),
+    contexts: ["image"],
+    onclick: (info, tab) => openLargeImage(info, tab, getLargeDbImageUrl),
+    documentUrlPatterns: [
+        "*://*.dagbladet.no/*"
     ]
 });
