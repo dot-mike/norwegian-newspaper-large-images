a890a1d7e39875432ebd2439e0d96221ba0b6431
Halvor Holsten Strand <halvor.holsten.strand@gmail.com>
2019-11-01 17:34:51 +0100

    Added Firefox deploy functionality. Needs better validation, but currently submits.

--- index.js
+++ index.js
@@ -1,8 +1,9 @@
 const fs = require("fs");
 const archiver = require("archiver");
 
-require('dotenv').config();
 const ZIP_FILENAME = "norwegian-newspaper-large-images.zip";
+const SOURCE_FOLDER = "src";
+const DIST_FOLDER = "dist";
 
 async function zipDirectory(source, out) {
   const archive = archiver("zip", { zlib: { level: 9 } });
@@ -37,21 +38,42 @@ async function publishToChrome(zipFilePath) {
 
   const token = await webStore.fetchToken();
   result = await webStore.uploadExisting(zipFile, token);
-  console.log("Finished uploading:", result);
+  console.log("Finished uploading to Chrome:", result);
   if (result.uploadState != "SUCCESS") {
     throw new Error(result);
   }
 
   result = await webStore.publish('default', token);
-  console.log("Finished publishing:", result);
+  console.log("Finished publishing to Chrome:", result);
   if (result.status[0] != 'OK') {
     throw new Error(result);
   }
 }
 
+async function publishToFirefox(sourcePath, artifactsPath) {
+  const webExt = require('web-ext').default;
+
+  webExt.cmd.sign({
+    sourceDir: sourcePath,
+    artifactsDir: artifactsPath,
+    channel: 'listed',
+    id: process.env.WEB_EXT_ID,
+    apiKey: process.env.WEB_EXT_API_KEY,
+    apiSecret: process.env.WEB_EXT_API_SECRET
+  }, {
+    shouldExitProgram: false,
+  }).then(reason => {
+    console.info("Finished publishing to Firefox:", reason);
+  }).catch(reason => {
+    console.info("Finished publishing to Firefox:", reason);
+  });
+}
+
 async function processDeploy() {
-  await zipDirectory("./src", `./${ZIP_FILENAME}`);
-  await publishToChrome(`./${ZIP_FILENAME}`);
+  require('dotenv').config();
+  await zipDirectory(`./${SOURCE_FOLDER}`, `./${DIST_FOLDER}/${ZIP_FILENAME}`);
+  await publishToChrome(`./${DIST_FOLDER}/${ZIP_FILENAME}`);
+  await publishToFirefox(SOURCE_FOLDER, DIST_FOLDER);
 }
 
 processDeploy().catch(err => {
